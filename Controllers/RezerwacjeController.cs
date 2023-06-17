379274ad1e0b3761c3cbc2ea0098fa386ec343d9
ukasz <chmiel11233@gmail.com>
2023-06-17 22:11:01 +0200

    Finished basic functionality

--- Controllers/RezerwacjeController.cs
+++ Controllers/RezerwacjeController.cs
@@ -22,21 +22,22 @@ namespace RezerwacjaBoiska.Controllers
         // GET: Rezerwacje
         public async Task<IActionResult> Index()
         {
+            var rezerwacja = _context.Rezerwacje.Include(p => p.Boiska).Include(p => p.Gracze).AsNoTracking();
               return _context.Rezerwacje != null ? 
-                          View(await _context.Rezerwacje.ToListAsync()) :
+                          View(await rezerwacja.ToListAsync()) :
                           Problem("Entity set 'RezerwacjaBoiskaContext.Rezerwacje'  is null.");
         }
 
         public IActionResult Informacje()
         {
             DateTime dzisiaj = DateTime.Today;
-            List<Rezerwacje> rezerwacjeAnulowane = _context.Rezerwacje
+            List<Rezerwacje> rezerwacjeAnulowane = _context.Rezerwacje.Include(p => p.Boiska).Include(p => p.Gracze)
                 .Where(r => r.Status == StatusRezerwacji.Anulowana)
                 .ToList();
-            List<Rezerwacje> rezerwacjeWTrakcie = _context.Rezerwacje
+            List<Rezerwacje> rezerwacjeWTrakcie = _context.Rezerwacje.Include(p => p.Boiska).Include(p => p.Gracze)
                 .Where(r => r.Status == StatusRezerwacji.WTrakcie)
                 .ToList();
-            List<Rezerwacje> rezerwacjeDzisiaj = _context.Rezerwacje
+            List<Rezerwacje> rezerwacjeDzisiaj = _context.Rezerwacje.Include(p => p.Boiska).Include(p => p.Gracze)
                 .Where(r => r.DataRezerwacji.Date == dzisiaj)
                 .ToList();
 
@@ -60,6 +61,8 @@ namespace RezerwacjaBoiska.Controllers
             }
 
             var rezerwacje = await _context.Rezerwacje
+                .Include(p => p.Gracze)
+                .Include(p => p.Boiska)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (rezerwacje == null)
             {
@@ -71,32 +74,28 @@ namespace RezerwacjaBoiska.Controllers
 
         private void PopulateGraczeDropDownList(object selectedGracz = null)
         {
-            var wybraneEtaty = from e in _context.Gracz
+            var wybraniGracze = from e in _context.Gracz
                                 orderby e.Imie
                                 select e;
-            var res = wybraneEtaty.AsNoTracking();
-            ViewBag.EtatyID = new SelectList(res, "Id", "Nazwa", selectedGracz);
+            var res = wybraniGracze.AsNoTracking();
+            ViewBag.GraczeID = new SelectList(res, "Id", "Imie", selectedGracz);
         }
 
         private void PopulateBoiskaDropDownList(object selectedBoisko = null)
         {
-            var wybraneEtaty = from e in _context.Boiska
+            var wybraneBoiska = from e in _context.Boiska
                                 orderby e.Nazwa
                                 select e;
-            var res = wybraneEtaty.AsNoTracking();
-            ViewBag.ZespolyID = new SelectList(res, "Id", "Nazwa", selectedBoisko);
+            var res = wybraneBoiska.AsNoTracking();
+            ViewBag.BoiskaID = new SelectList(res, "Id", "Nazwa", selectedBoisko);
         }
         // GET: Rezerwacje/Create
         public IActionResult Create()
         {
             var availableStatuses = Enum.GetNames(typeof(StatusRezerwacji));
-            foreach(var stat in availableStatuses)
-            {
-                Console.WriteLine(stat);
-            }
+            ViewBag.AvailableStatuses = new SelectList(availableStatuses);
             PopulateGraczeDropDownList();
             PopulateBoiskaDropDownList();
-            ViewBag.AvailableStatuses = new SelectList(availableStatuses);
             return View();
         }
 
@@ -105,11 +104,14 @@ namespace RezerwacjaBoiska.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,DataRezerwacji,GodzinaRozpoczecia,GodzinaZakonczenia,Status")] Rezerwacje rezerwacje,
+        public async Task<IActionResult> Create([Bind("Id,DataRezerwacji,GodzinaRozpoczecia,GodzinaZakonczenia,Status")] 
+        Rezerwacje rezerwacje,
         IFormCollection form)
         {
-            string graczValue = form["Gracz"].ToString();
-            string boiskoValue = form["Zespol"].ToString();
+            string graczValue = form["Gracze"].ToString();
+            string boiskoValue = form["Boiska"].ToString();
+            Console.WriteLine(graczValue);
+            Console.WriteLine(boiskoValue);
             if (ModelState.IsValid)
             {
                 Gracz gracz = null;
@@ -128,8 +130,8 @@ namespace RezerwacjaBoiska.Controllers
                 }
                 rezerwacje.Gracze = gracz;
                 rezerwacje.Boiska = boisko;
-                _context.Add(rezerwacje);
 
+                _context.Add(rezerwacje);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
@@ -139,17 +141,37 @@ namespace RezerwacjaBoiska.Controllers
         // GET: Rezerwacje/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
+            var availableStatuses = Enum.GetNames(typeof(StatusRezerwacji));
+            ViewBag.AvailableStatuses = new SelectList(availableStatuses);
             if (id == null || _context.Rezerwacje == null)
             {
                 return NotFound();
             }
 
-            var rezerwacje = await _context.Rezerwacje.FindAsync(id);
-            if (rezerwacje == null)
+            var rezerwacja = _context.Rezerwacje.Where(p => p.Id == id)
+                .Include(p => p.Gracze).Include(p => p.Boiska).First();
+            if (rezerwacja == null)
             {
                 return NotFound();
             }
-            return View(rezerwacje);
+            if(rezerwacja.Gracze != null)
+            {
+                PopulateGraczeDropDownList(rezerwacja.Gracze.Id);
+            }
+            else
+            {
+                PopulateGraczeDropDownList();
+            }
+
+            if(rezerwacja.Boiska != null)
+            {
+                PopulateBoiskaDropDownList(rezerwacja.Boiska.Id);
+            }
+            else
+            {
+                PopulateBoiskaDropDownList();
+            }
+            return View(rezerwacja);
         }
 
         // POST: Rezerwacje/Edit/5
@@ -157,7 +179,8 @@ namespace RezerwacjaBoiska.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,DataRezrwacji,GodzinaRozpoczecia,GodzinaZakonczenia,Status")] Rezerwacje rezerwacje)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DataRezrwacji,GodzinaRozpoczecia,GodzinaZakonczenia,Status")] Rezerwacje rezerwacje,
+        IFormCollection form)
         {
             if (id != rezerwacje.Id)
             {
@@ -168,7 +191,41 @@ namespace RezerwacjaBoiska.Controllers
             {
                 try
                 {
-                    _context.Update(rezerwacje);
+                    String graczValue = form["Gracze"];
+                    String boiskoValue = form["Boiska"];
+                    Gracz gracz = null;
+                    if(graczValue != "-1")
+                    {
+                        var ee = _context.Gracz.Where(e => e.Id == int.Parse(graczValue));
+                        if(ee.Count() > 0)
+                        {
+                            gracz = ee.First();
+                        }
+                    }
+                    Boiska boisko = null;
+                    if(boiskoValue != "-1")
+                    {
+                        var ee = _context.Boiska.Where(e => e.Id == int.Parse(boiskoValue));
+                        if(ee.Count() > 0)
+                        {
+                            boisko = ee.First();
+                        }
+                    }
+
+                    rezerwacje.Gracze = gracz;
+                    rezerwacje.Boiska = boisko;
+
+                    Rezerwacje pp = _context.Rezerwacje.Where(p => p.Id == id)
+                    .Include(p => p.Boiska)
+                    .Include(p => p.Gracze)
+                    .First();
+                    pp.Gracze = gracz;
+                    pp.Boiska = boisko;
+                    pp.DataRezerwacji = rezerwacje.DataRezerwacji;
+                    pp.GodzinaRozpoczecia = rezerwacje.GodzinaRozpoczecia;
+                    pp.GodzinaZakonczenia = rezerwacje.GodzinaZakonczenia;
+                    pp.Status = rezerwacje.Status;
+
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -195,14 +252,14 @@ namespace RezerwacjaBoiska.Controllers
                 return NotFound();
             }
 
-            var rezerwacje = await _context.Rezerwacje
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (rezerwacje == null)
+            var rezerwacja = _context.Rezerwacje.Where(p => p.Id == id)
+                .Include(p => p.Gracze).Include(p => p.Boiska).First();
+            if (rezerwacja == null)
             {
                 return NotFound();
             }
 
-            return View(rezerwacje);
+            return View(rezerwacja);
         }
 
         // POST: Rezerwacje/Delete/5
